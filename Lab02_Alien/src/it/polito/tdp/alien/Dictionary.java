fe7a929909d7edd380b887e447d93a1e562e3c11
Elia Daniotti <Elia Daniotti@DESKTOP-55HFKSK>
2019-04-30 17:49:25 +0200

    word

---
+++ Lab02_Alien/src/it/polito/tdp/alien/Dictionary.java
@@ -0,0 +1,6 @@
+package it.polito.tdp.alien;
+
+public class Dictionary {
+	
+
+}
---
+++ Lab02_Alien/src/it/polito/tdp/alien/Word.java
@@ -0,0 +1,23 @@
+package it.polito.tdp.alien;
+
+public class Word {
+	
+	private String alienWord;
+	private String translation;
+	
+	public String getAlienWord() {
+		return alienWord;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	
+	
+
+}
